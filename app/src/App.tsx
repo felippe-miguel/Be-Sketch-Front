3849d0f11d4c16df11ac7f6e786d13f14418a0a0
Felippe Miguel <felippe.miguel.silva@gmail.com>
2022-06-04 16:29:40 -0300

    adding reactQuery

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,27 @@
+import {useQuery} from "react-query"
+import axios from "axios"
+
+type Profile = {
+  "id": number,
+  "name": string,
+  "email": string
+}
+
 function App() {
-  return <h1>Hello world</h1>
+  const { data: profile, isFetching } = useQuery<Profile>("profile", async () => {
+    const response = await axios.get("http://localhost:8080/api/profile")
+
+    return response.data
+  })
+
+  return (
+    <>
+      { isFetching && <p>Carregando...</p> }
+      <h1>Nome: {profile?.name}</h1>
+      <p>id: {profile?.id}</p>
+      <p>email: {profile?.email}</p>
+    </>
+  )
 }
 
 export default App
