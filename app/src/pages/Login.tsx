b1d4885167040368c10d19f576c6cd11932be3fe
Felippe Miguel <felippe.miguel.silva@gmail.com>
2022-06-06 14:26:49 -0300

    creating loginForm component

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,3 +1,9 @@
+import LoginForm from "../components/LoginForm";
+
 export default function Login() {
-    return <h1>Login</h1>
+  const handleLogin = (data: {"email": string, "password": string}) => {
+    console.log(data)
+  }
+
+  return <LoginForm login={handleLogin} />
 }
