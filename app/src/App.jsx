3b1c697372d2f83ebfef066a033eb488ca9a257f
Felippe Miguel <felippe.miguel.silva@gmail.com>
2022-06-08 20:52:01 -0300

    adjusts in app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-import Profile from './pages/Profile'
+import { Login } from './components/Login'
+import { Profile } from './components/Profile'
+import { AuthProvider } from './services/auth'
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/login' element={ <Login /> } />
-      <Route path='/profile' element={ <Profile /> } />
-    </Routes>
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='login' element={ <Login /> } />
+        <Route path='profile' element={ <Profile /> } />
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>
 )
 
 export default App
