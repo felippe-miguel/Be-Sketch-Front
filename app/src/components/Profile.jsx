b45479fb830f68338c20c705387c34df068e9101
Felippe Miguel <felippe.miguel.silva@gmail.com>
2022-06-08 20:51:16 -0300

    new profile page

---
+++ app/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../services/auth"
+
+export const Profile = () => {
+  const auth = useAuth()
+  const navigate = useNavigate
+
+  const handleLogout = () => {
+    auth.logout()
+    navigate("login")
+  }
+
+  return (
+    <>
+      <h1>profile</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </>
+  )
+}
