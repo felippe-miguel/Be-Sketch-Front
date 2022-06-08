5918153f31ce5989856192c2b4329bdab9983dff
Felippe Miguel <felippe.miguel.silva@gmail.com>
2022-06-07 21:09:33 -0300

    removing typescript

--- app/src/pages/Login.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "../components/LoginForm";
-
-export default function Login() {
-  const handleLogin = (data: {"email": string, "password": string}) => {
-    console.log(data)
-  }
-
-  return <LoginForm login={handleLogin} />
-}
